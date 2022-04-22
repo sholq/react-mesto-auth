fbbb3d7fbaa518118b8a1186b83b46566a2439f2
sholq <semyon.silkin@gmail.com>
2022-04-22 10:05:54 +0300

    fix: Rev.1

--- src/components/App.js
+++ src/components/App.js
@@ -68,70 +68,54 @@ function App() {
       <PopupWithForm
         title='Редактировать профиль'
         name='edit'
+        buttonText='Сохранить'
         isOpen={isEditProfilePopupOpen}
         onClose={() => {
           setIsEditProfilePopupOpen(false);
-        }}
-        children={
-          <>
-            <label className="popup__field">
-              <input className="popup__input popup__input_type_name" type="text" name="name" value="" autocomplete="off" id="edit-popup-name-input" minlength="2" maxlength="40" required />
-              <span className="popup__input-error edit-popup-name-input-error"></span>
-            </label>
-            <label className="popup__field">
-              <input className="popup__input popup__input_type_description" type="text" name="about" value="" autocomplete="off" id="edit-popup-description-input"  minlength="2" maxlength="200" required />
-              <span className="popup__input-error edit-popup-description-input-error"></span>
-            </label>
-            <button className="popup__save-button" type="submit">Сохранить</button>
-          </>
-        }
-      />
+        }}>
+         <label className="popup__field">
+            <input className="popup__input popup__input_type_name" type="text" name="name" value="" placeholder="Имя" autocomplete="off" id="edit-popup-name-input" minlength="2" maxlength="40" required />
+            <span className="popup__input-error edit-popup-name-input-error"></span>
+          </label>
+          <label className="popup__field">
+            <input className="popup__input popup__input_type_description" type="text" name="about" value="" placeholder="О себе" autocomplete="off" id="edit-popup-description-input"  minlength="2" maxlength="200" required />
+            <span className="popup__input-error edit-popup-description-input-error"></span>
+          </label>
+      </PopupWithForm>
       <PopupWithForm
         title='Новое место'
         name='add'
+        buttonText='Создать'
         isOpen={isAddPlacePopupOpen}
         onClose={() => {
           setIsAddPlacePopupOpen(false);
-        }}
-        children={
-          <>
-            <label className="popup__field">
-              <input className="popup__input popup__input_type_name" type="text" name="name" placeholder="Название" autocomplete="off" id="add-popup-name-input" minlength="2" maxlength="30" required />
-              <span className="popup__input-error add-popup-name-input-error"></span>
-            </label>
-            <label className="popup__field">
-              <input className="popup__input popup__input_type_link" type="url" name="link" placeholder="Ссылка на картинку" autocomplete="off" id="add-popup-link-input" required />
-              <span className="popup__input-error add-popup-link-input-error"></span>
-            </label>
-            <button className="popup__save-button" type="submit">Создать</button>
-          </>
-        }
-      />
+        }}>
+          <label className="popup__field">
+            <input className="popup__input popup__input_type_name" type="text" name="name" placeholder="Название" autocomplete="off" id="add-popup-name-input" minlength="2" maxlength="30" required />
+            <span className="popup__input-error add-popup-name-input-error"></span>
+          </label>
+          <label className="popup__field">
+            <input className="popup__input popup__input_type_link" type="url" name="link" placeholder="Ссылка на картинку" autocomplete="off" id="add-popup-link-input" required />
+            <span className="popup__input-error add-popup-link-input-error"></span>
+          </label>
+      </PopupWithForm>
       <PopupWithForm
         title='Обновить аватар'
         name='avatar'
+        buttonText='Сохранить'
         isOpen={isEditAvatarPopupOpen}
         onClose={() => {
           setIsEditAvatarPopupOpen(false);
-        }}
-        children={
-          <>
-            <label className="popup__field">
-              <input className="popup__input popup__input_type_link" type="url" name="avatar" placeholder="Ссылка на картинку" autocomplete="off" id="edit-avatar-popup-link-input" required />
-              <span className="popup__input-error edit-avatar-popup-link-input-error"></span>
-            </label>
-            <button className="popup__save-button" type="submit">Сохранить</button>
-          </>
-        }
-      />
+      }}>
+        <label className="popup__field">
+          <input className="popup__input popup__input_type_link" type="url" name="avatar" placeholder="Ссылка на картинку" autocomplete="off" id="edit-avatar-popup-link-input" required />
+          <span className="popup__input-error edit-avatar-popup-link-input-error"></span>
+        </label>
+      </PopupWithForm>
       <PopupWithForm
         title='Вы уверены?'
         name='confirm'
-        children={
-          <>
-            <button className="popup__save-button popup__save-button_type_confirm">Да</button>
-          </>
-        }
+        buttonText='Да'
       />
       <ImagePopup
         card={selectedCard}
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,6 +5,7 @@ function PopupWithForm(props) {
         <h2 className="popup__header">{props.title}</h2>
         <form className="popup__form" name={`${props.name}PopupForm`}>
           {props.children}
+          <button className="popup__save-button" type="submit">{props.buttonText}</button>
         </form>
         <button className="popup__close-button" type="button" aria-label="Закрыть" onClick={props.onClose}></button>
         </div>
