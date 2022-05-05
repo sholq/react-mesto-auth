0195a8a76b005d4c80d232e8264c77c15a5bec71
sholq <semyon.silkin@gmail.com>
2022-05-05 19:16:20 +0300

    fix: fix validation

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,17 +1,24 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function PopupWithForm(props) {
     const {name, title, children, buttonText, isOpen, onClose, onSubmit, isValid, isEmpty} = props;
 
-    const form = useRef();
+    const button = useRef();
+
+    const [isInactive, setIsInactive] = useState(isEmpty || !isValid);
+
+    useEffect(() => {
+      setIsInactive(isEmpty || !isValid);
+      (isInactive) ? button.current.setAttribute("disabled", "disabled") : button.current.removeAttribute("disabled");
+    }, [isValid, isEmpty, isInactive])
 
     return (
       <div className={`popup popup_type_${name}` + (isOpen ? " popup_opened" : "")}>
         <div className="popup__container">
         <h2 className="popup__header">{title}</h2>
-        <form ref={form} className="popup__form" name={`${name}PopupForm`} onSubmit={onSubmit}>
+        <form className="popup__form" name={`${name}PopupForm`} onSubmit={onSubmit}>
           {children}
-          <button className={"popup__save-button" + (isEmpty || !isValid ? " popup__save-button_inactive" : "")} type="submit">{buttonText}</button>
+          <button ref={button} className={"popup__save-button" + (isInactive ? " popup__save-button_inactive" : "")} type="submit">{buttonText}</button>
         </form>
         <button className="popup__close-button" type="button" aria-label="Закрыть" onClick={onClose}></button>
         </div>
