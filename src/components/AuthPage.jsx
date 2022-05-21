ff83795930667fda7fb98159f877a515992661ed
sholq <semyon.silkin@gmail.com>
2022-05-21 20:02:19 +0300

    refactor: change auth forms validation order

--- src/components/AuthPage.jsx
+++ src/components/AuthPage.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 
 function AuthPage(props) {
-    const {name, title, children, buttonText, onSubmit, isValid, isEmpty, isSignIn} = props;
+    const {name, title, children, buttonText, onSubmit, isSignIn} = props;
 
     return (
       <section className="auth">
         <h2 className="auth__header">{title}</h2>
-        <form className="auth__form" name={name} onSubmit={onSubmit}>
+        <form className="auth__form" name={name} onSubmit={onSubmit} noValidate={true}>
           {children}
-          <button className={"auth__save-button" + (isEmpty || !isValid ? " auth__save-button_inactive" : "")} type="submit" disabled={isEmpty || !isValid}>{buttonText}</button>
+          <button className={"auth__save-button"} type="submit">{buttonText}</button>
         </form>
         {isSignIn && <Link className="auth__link" to="/sign-up">Уже зарегистрированы? Войти</Link>}
       </section>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,45 +8,37 @@ function Login(props) {
   const password = useRef();
 
   const [isEmailValid, setIsEmailValid] = useState(true);
-  const [isEmailEmpty, setIsEmailEmpty] = useState(true);
   const [emailValidationMessage, setEmailValidationMessage] = useState('');
   
   const [isPasswordValid, setIsPasswordValid] = useState(true);
-  const [isPasswordEmpty, setIsPasswordEmpty] = useState(true);
   const [passwordValidationMessage, setPasswordValidationMessage] = useState('');
   
   const [buttonText, setButtonText] = useState('Войти');
-
-  const handleEmailChange = (evt) => {
-    setIsEmailValid(evt.target.validity.valid);
-    setEmailValidationMessage(evt.target.validationMessage);
-    (evt.target.value === '') ? setIsEmailEmpty(true) : setIsEmailEmpty(false);
-  }
-
-  const handlePasswordChange = (evt) => {
-    setIsPasswordValid(evt.target.validity.valid);
-    setPasswordValidationMessage(evt.target.validationMessage);
-    (evt.target.value === '') ? setIsPasswordEmpty(true) : setIsPasswordEmpty(false);
-  }
   
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    setButtonText(buttonText + '...');
-    onLogin(email.current.value, password.current.value)
-      .then(res => {
-        if (res) {
-          email.current.value = '';
-          password.current.value = '';
-        }
-      })
-      .finally(res => {
-        setButtonText(buttonText);
-        if (!res) {
-          setEmailValidationMessage('Неверный адрес или пароль');
-          setIsEmailValid(false);
-          setIsPasswordValid(false);
-        }
-      });
+    setIsEmailValid(email.current.validity.valid);
+    setEmailValidationMessage(email.current.validationMessage);
+    setIsPasswordValid(password.current.validity.valid);
+    setPasswordValidationMessage(password.current.validationMessage);
+    if (email.current.validity.valid && password.current.validity.valid) {
+      setButtonText(buttonText + '...');
+      onLogin(email.current.value, password.current.value)
+        .then(res => {
+          if (res) {
+            email.current.value = '';
+            password.current.value = '';
+          }
+        })
+        .finally(res => {
+          setButtonText(buttonText);
+          if (!res) {
+            setEmailValidationMessage('Неверный адрес или пароль');
+            setIsEmailValid(false);
+            setIsPasswordValid(false);
+          }
+        });
+    }
   }
 
   useEffect(() => {
@@ -55,9 +47,7 @@ function Login(props) {
     setEmailValidationMessage('');
     setPasswordValidationMessage('');
     setIsEmailValid(true);
-    setIsEmailEmpty(true);
     setIsPasswordValid(true);
-    setIsPasswordEmpty(true);
   }, []);
 
   return (
@@ -65,16 +55,14 @@ function Login(props) {
       name="login"
       title="Вход"
       buttonText={buttonText}
-      isValid={isEmailValid && isPasswordValid}
-      isEmpty={isEmailEmpty || isPasswordEmpty}
       onSubmit={handleSubmit}
     >
       <label className="auth__field">
-        <input ref={email} className={"auth__input" + (!isEmailValid ? " auth__input_invalid" : "")} type="text" name="email" placeholder="E-mail" autoComplete="off" minLength="2" maxLength="30" required onChange={handleEmailChange}/>
+        <input ref={email} className={"auth__input" + (!isEmailValid ? " auth__input_invalid" : "")} type="text" name="email" placeholder="E-mail" autoComplete="off" minLength="2" maxLength="30" required />
         <span className="auth__input-error">{emailValidationMessage}</span>
       </label>
       <label className="auth__field">
-        <input ref={password} className={"auth__input" + (!isPasswordValid ? " auth__input_invalid" : "")} type="password" name="password" placeholder="Пароль" autoComplete="off" minLength="6" maxLength="30" required onChange={handlePasswordChange}/>
+        <input ref={password} className={"auth__input" + (!isPasswordValid ? " auth__input_invalid" : "")} type="password" name="password" placeholder="Пароль" autoComplete="off" minLength="6" maxLength="30" required />
         <span className="auth__input-error">{passwordValidationMessage}</span>
       </label>
     </ AuthPage>
